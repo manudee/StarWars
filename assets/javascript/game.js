50d9ec68a0efd794d2ce83152ff3e8ff1c5eca2c
Manasi <manudee@gmail.com>
2017-09-17 20:33:43 -0700

    Added attack function basic structure and created character objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 
+
+/*
 $(document).ready(function(){
 
 		$(".allChar").on("click", function(){
@@ -9,7 +11,7 @@ $(document).ready(function(){
 
 				$("#yourCharacter").append(yourChar);
 
-				var enemiesChar=$('#characters>button').siblings();
+				var enemiesChar=$("#characters>button").siblings();
 				console.log("siblings");
 
 				enemiesChar.appendTo("#enemies").removeClass("allChar").addClass("enemiesArea");
@@ -21,6 +23,8 @@ $(document).ready(function(){
 					var defenderChar = $(this);
 					console.log(defenderChar);
 					$("#defenderArea").append(defenderChar);
+					$(".enemiesArea>button").prop("disabled",true);
+
 				});
 
 
@@ -28,6 +32,78 @@ $(document).ready(function(){
 
 		});
 
+
+
+});*/
+
+
+$(document).ready(function(){
+
+
+var characters  = {
+		 "obi" : {
+
+			"healthPoints": 120,
+			"attackPoints": 8,
+			"counterAttackPoints": 25
+		},
+
+
+		 "luke" : {
+
+			"healthPoints": 100,
+			"attackPoints": 20,
+			"counterAttackPoints": 10
+		},
+
+
+		 "darthSidious" : {
+
+			"healthPoints": 150,
+			"attackPoints": 25,
+			"counterAttackPoints": 30
+		},
+
+		 "darthMaul" : {
+
+			"healthPoints": 180,
+			"attackPoints": 10,
+			"counterAttackPoints": 40
+		}
+};
+
+
+
+		$("body").on("click", ".allChar", function(){
+				
+				$(this).siblings().appendTo("#enemies").removeClass("allChar").addClass("enemies");
+				console.log($(this).siblings());
+				$("#yourCharacter").append(this);
+
+		});//end of click event handler
+		
+		$("body").on("click", ".enemies", function(){
+	
+				$(this).appendTo('#defenderArea');
+				$(".enemiesArea>button").prop("disabled",true);
+		});//end of click event handler
+
+
+
+		$("body").on("click", "#attack", function(){
+
+			//need to get yourcharacter first that was clicked
+			var yourCharacter  = $("#yourCharacter>button").text();
+			var defender  = $("#defenderArea>button").text();	
+
+			console.log(yourCharacter);
+			console.log(defender);
+
+
+			console.log(characters[0]);
+
+
+		});
 
 
 });
