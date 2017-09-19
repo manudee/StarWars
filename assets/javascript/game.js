bae4b1d36ce6d1e8f0254ec69d4c82c8107579d3
Manasi <manudee@gmail.com>
2017-09-18 19:39:25 -0700

    Added images, logic for attack and restartGame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,32 +41,32 @@ $(document).ready(function(){
  
 var attackCount = 0;
 var characters  = {
-     	    obi : {
+     	    jon : {
  
-                "name": "obi",
+                "name":"jon",
                 "healthPoints": 120,
                 "attackPoints": 8,
                 "counterAttackPoints": 25
             },
  
  
-            luke : {
-            	"name": "luke",
+            daenerys : {
+            	"name":"daenerys",
             	"healthPoints": 100,
             	"attackPoints": 20,
             	"counterAttackPoints": 10
             },
 
 
-            darthSidious : {
-            	"name": "darthSidious",
+            nightKing : {
+            	"name":"nightKing",
             	"healthPoints": 150,
             	"attackPoints": 25,
             	"counterAttackPoints": 30
             },
 
-            darthMaul : {
-            	"name": "darthMaul",
+            cersei : {
+            	"name":"cersei",
             	"healthPoints": 180,
             	"attackPoints": 10,
             	"counterAttackPoints": 40
@@ -86,28 +86,31 @@ var characters  = {
         $("body").on("click", ".enemies", function(){
 
         	$(this).appendTo('#defenderArea');
-        	$(".enemiesArea>button").prop("disabled",true);
-                                });//end of click event handler
+        	$(".enemiesArea>img").prop("disabled",true);
+   		});//end of click event handler
 
 
 
         $("body").on("click", "#attack", function(){
-        	var temp;
+
+
+
+        		var temp;
                                                 //need to get yourcharacter first that was clicked
-				var yourCharacter  = $("#yourCharacter>button").text();
-				var defender  = $("#defenderArea>button").text();      
+				var yourCharacter  = $("#yourCharacter>img").attr('alt');
+				var defender  = $("#defenderArea>img:visible").attr('alt');      
 
 				console.log(yourCharacter);
 				console.log(defender);
 
 				console.log(characters[yourCharacter].healthPoints);
+				console.log(characters[defender].attackPoints);
 
+				//var myChar = yourCharacter.split(".");
+				//var defenderChar = defender.split(".");
 
-				var myChar = yourCharacter.split(".");
-				var defenderChar = defender.split(".");
-
-				console.log(myChar);
-				console.log(defenderChar);
+				//console.log(myChar);
+				//console.log(defenderChar);
 
 
 				//console.log(characters[myChar].healthPoints);
@@ -120,51 +123,62 @@ var characters  = {
 
                                                 
                 console.log("for the first attack");
-                console.log(characters[myChar].healthPoints);
-                console.log(characters[defenderChar].healthPoints);
-                console.log(characters[myChar].attackPoints);
-                console.log(characters[defenderChar].attackPoints);
+                console.log(characters[yourCharacter].healthPoints);
+                console.log(characters[defender].healthPoints);
+                console.log(characters[yourCharacter].attackPoints);
+                console.log(characters[defender].attackPoints);
                 console.log("attackCount Before " + attackCount);
                 attackCount++;
-                temp = characters[myChar].attackPoints * attackCount;              
+                temp = characters[yourCharacter].attackPoints * attackCount;              
                 console.log("temp " + temp);
 
-                characters[defenderChar].healthPoints = characters[defenderChar].healthPoints - temp;
+                characters[defender].healthPoints = characters[defender].healthPoints - temp;
 
 
 
 
-                characters[myChar].healthPoints = characters[myChar].healthPoints - characters[defenderChar].attackPoints;
+                characters[yourCharacter].healthPoints = characters[yourCharacter].healthPoints - characters[defender].attackPoints;
 
                 console.log("State after the first attack");
-                console.log(characters[myChar].healthPoints);
-                console.log(characters[defenderChar].healthPoints);
+                console.log(characters[yourCharacter].healthPoints);
+                console.log(characters[defender].healthPoints);
                 console.log(temp);
-                console.log(characters[defenderChar].attackPoints);
+                console.log(characters[defender].attackPoints);
 
-                console.log("You attacked " + defenderChar + "for damage of " + temp );
-                console.log(defenderChar + "attacked you back " + " for damage of " + characters[defenderChar].attackPoints );
+                console.log("You attacked " + characters[defender].name + " for damage of " + temp );
+                console.log(characters[defender].name + " attacked you back " + " for damage of " + characters[defender].attackPoints );
 
                 console.log("attackCount " + attackCount);
 
 
-                if(characters[defenderChar].healthPoints < 0 ){
+                if(characters[defender].healthPoints < 0 ){
                 	console.log("You win");
-                	console.log("You have defeated " + defenderChar + "." + "You can choose to fight another enemy" );
-                	$(".enemiesArea>button").prop("disabled",false);
-                                                // and enemies not reached 0
-                                                                //call for selection of another defender
-            }             
-          //else if(characters[myChar].healthPoints < 0)
-                                                                //call for resetting the game
-                                                                //disable attach button
-                                                                //restart the game
+                	console.log("You have defeated " + characters[defender].name + "." + "You can choose to fight another enemy" );
+                		
+                	$("#defenderArea>img").hide();
+                	$(".enemiesArea>img").prop("disabled",false);
+                     // and enemies not reached 0
+                     //call for selection of another defender
+           		 }             
+         				 //else if(characters[myChar].healthPoints < 0)
+                         //call for resetting the game
+                         //disable attach button
+                         //restart the game
+						//	}
 
 
-                                //            }
+        });
 
 
-                            });
+
+        $("body").on("click", "#restartGame", function(){
+        	$("#defenderArea>img").show();
+        	//$("#defenderArea").appendTo("#yourCharacter");
+        	$("#yourCharacter>img").appendTo("#characters");
+        	$("#defenderArea>img").appendTo("#characters").removeClass("enemies").addClass("allChar");
+
+
+        });
 
 
     });
